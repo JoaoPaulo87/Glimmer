597e367e08749a58a7bb3523832178ef1304845b
JoaoPaulo87 <correaparisejoaopaulo@gmail.com>
2020-02-08 21:20:28 -0300

    Player and raw level design

---
+++ Glimmer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;               
+
+    private Rigidbody playerRB;  
+
+    void Start()
+    {
+        this.playerRB = gameObject.GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        playerRB.AddForce(movement * speed);
+    }
+}
